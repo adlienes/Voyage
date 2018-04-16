d68618a39c0c2a7aa4c6bbe4259e3fb27c8fb87d
adlienes <adli_enes@hotmail.com>
2018-04-17 01:03:41 +0300

    add Read Data

--- IosProjem/Controller/IlanlarViewController.swift
+++ IosProjem/Controller/IlanlarViewController.swift
@@ -7,13 +7,33 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class IlanlarViewController: UIViewController {
-
+class IlanlarViewController: UIViewController,UITableViewDelegate, UITableViewDataSource   {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var postData = [String] ()
+    var ref:DatabaseReference?
+    var handle:DatabaseHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate=self
+        tableView.dataSource=self
         // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        handle=ref?.child("Gonderiler").observe(.childAdded, with: { (snapshot) in
+            
+            let post=snapshot.value as? String
+            
+            if let actualpost=post {
+                self.postData.append(actualpost)
+                self.tableView.reloadData()
+            }
+        })
     }
 
     @IBAction func BtnGeri(_ sender: UIBarButtonItem) {
@@ -22,6 +42,16 @@ class IlanlarViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = postData[indexPath.row]
+        return cell
     }
     
 
